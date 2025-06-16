ab6cd0056d413b93dc1c7cb3a23bdedf77633ed5
= <=>
2025-06-17 02:01:31 +0530

    MAJOR: updated to dark ui

--- app/Components/home/HeaderSocials.js
+++ app/Components/home/HeaderSocials.js
@@ -1,26 +1,49 @@
-
-import React from 'react'
+import React from 'react';
 
 const HeaderSocials = () => {
   return (
-  <div className='home__socials'>
-        <a href='https://www.instagram.com/_.h.a_r.i.__' className='home__social-link' target='_blank'>
-        <i class="fa-brands fa-instagram"></i>
-        </a>
+    <div className='home__socials'>
+      <a
+        href='https://www.instagram.com/_.h.a_r.i.__'
+        className='home__social-link'
+        target='_blank'
+        rel='noreferrer'
+        style={{ color: '#E1306C' }} // Instagram pink
+      >
+        <i className='fa-brands fa-instagram'></i>
+      </a>
 
-        <a href='https://x.com/hari_n05' className='home__social-link' target='_blank'>
-        <i class="fa-brands fa-twitter"></i>
-        </a>
-        
-        <a href='https://github.com/Hari-N-2005/' className='home__social-link' target='_blank'>
-        <i class="fa-brands fa-github"></i>
-        </a>
+      <a
+        href='https://x.com/hari_n05'
+        className='home__social-link'
+        target='_blank'
+        rel='noreferrer'
+        style={{ color: '#1DA1F2' }} // Twitter blue
+      >
+        <i className='fa-brands fa-twitter'></i>
+      </a>
 
-        <a href='https://www.linkedin.com/in/hari-n05/' className='home__social-link' target='_blank'>
-        <i class="fa-brands fa-linkedin"></i>
-        </a>
-  </div>
-  )
-}
+      <a
+        href='https://github.com/Hari-N-2005/'
+        className='home__social-link'
+        target='_blank'
+        rel='noreferrer'
+        style={{ color: '#ffffff' }} // White for GitHub
+      >
+        <i className='fa-brands fa-github'></i>
+      </a>
 
-export default HeaderSocials
+      <a
+        href='https://www.linkedin.com/in/hari-n05/'
+        className='home__social-link'
+        target='_blank'
+        rel='noreferrer'
+        style={{ color: '#0077B5' }} // LinkedIn blue
+      >
+        <i className='fa-brands fa-linkedin'></i>
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
--- app/Components/sidebar/Sidebar.js
+++ app/Components/sidebar/Sidebar.js
@@ -2,68 +2,60 @@
 import React, { useState } from 'react';
 import "./Sidebar.css";
 
-
 const Sidebar = () => {
-
   const [toggle, showMenu] = useState(false);
+
   return (
-   <>
-    <aside className={toggle ? "aside show-menu" : "aside"}>
-    <a href='#home' className='nav__logo'>
-    <img src='https://static.vecteezy.com/system/resources/previews/006/719/267/non_2x/smiling-young-man-avatar-in-flat-cartoon-style-cute-male-character-boy-portrait-isolated-on-white-background-for-social-media-web-chat-vector.jpg' alt='sideLogo' />
-    </a>
+    <>
+      <aside className={toggle ? "aside show-menu" : "aside"}>
+        <nav className='nav'>
+          <div className='nav__menu'>
+            <ul className='nav__list'>
+              <li className='nav__item'>
+                <a href='#home' className='nav__link'>
+                  <i className="fa-solid fa-house"></i>
+                  <span className='nav__text'>Home</span>
+                </a>
+              </li>
 
-    <nav className='nav'>
-      <div className='nav__menu'>
-      <ul className='nav__list'>
-        <li className='nav__item'>
+              <li className='nav__item'>
+                <a href='#about' className='nav__link'>
+                  <i className="fa-solid fa-address-card"></i>
+                  <span className='nav__text'>About</span>
+                </a>
+              </li>
 
-          <a href='#home' className='nav__link'>
-             <i className="fa-solid fa-house"></i>
-             <span className='nav__text'>Home</span>
-          </a>
-        </li>
+              <li className='nav__item'>
+                <a href='#skills' className='nav__link'>
+                  <i className="fa-solid fa-desktop"></i>
+                  <span className='nav__text'>Skills</span>
+                </a>
+              </li>
 
-        <li className='nav__item'>
-          <a href='#about' className='nav__link'>
-            <i className="fa-solid fa-address-card"></i>
-            <span className='nav__text'>About</span>
-          </a>
-        </li>
+              <li className='nav__item'>
+                <a href='#work' className='nav__link'>
+                  <i className="fa-solid fa-diagram-project"></i>
+                  <span className='nav__text'>Projects</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
 
-        <li className='nav__item'>
-          <a href='#skills' className='nav__link'>
-            <i className="fa-solid fa-desktop"></i>
-            <span className='nav__text'>Skills</span>
-          </a>
-        </li>
+        <div className='nav__footer'>
+          <span className='copyright'>&copy;</span>
+          <span className='copyright'>&copy;.</span>
+        </div>
+      </aside>
 
-        <li className='nav__item'>
-          <a href='#work' className='nav__link'>
-            <i className="fa-solid fa-diagram-project"></i>
-            <span className='nav__text'>Projects</span>
-          </a>
-        </li>
-
-
-      </ul>
-
+      <div
+        className={toggle ? "nav__toggle nav__toggle-open" : "nav__toggle"}
+        onClick={() => showMenu(!toggle)}
+      >
+        <i className='icon-menu'></i>
       </div>
-    </nav>
-
-    <div className='nav__footer'>
-      <span className='copyright'>&copy;</span>
-      <span className='copyright'>&copy;.</span>
-    </div>
-
-   </aside>
-
-
-   <div className={toggle ? "nav__toggle nav__toggle-open" : "nav__toggle"} onClick={() => showMenu(!toggle)}>
-    <i className='icon-menu'></i>
-   </div>
-   </>
+    </>
   );
-}
+};
 
-export default Sidebar
+export default Sidebar;
